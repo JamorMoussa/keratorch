fa20c6a8cebcdcad26c6c1c344441e60dec14831
jamormoussa <moussajamorsup@gmail.com>
2024-11-16 01:56:00 +0100

    change the test module

--- test/test_module.py
+++ test/test_module.py
@@ -6,36 +6,22 @@ import keratorch as kt
 import matplotlib.pyplot as plt
 
 
-# class MLPModel(kt.nn.TrModule):
-
-#     def __init__(self):
-#         super().__init__()
-
-#         self.fc = nn.Linear(3, 1)
-
-#     def forward(self, x: tr.Tensor):
-#         return self.fc(x)
-
-# model = MLPModel()
-
-
-
-model = kt.nn.TrSequential(
-    nn.Linear(3, 1)
+model = kt.nn.Sequential(
+    kt.nn.Lambda(lambda x: x.unsqueeze(1)),
+    nn.Conv1d(1, 10, kernel_size=2),
+    nn.Flatten(start_dim=1),
+    nn.Linear(10 * 6 , 1)
 )
-
-print(model.__class__)
-
 
 model.compile(
     loss_fn= nn.MSELoss(),
-    optimizer= tr.optim.Adam(model.parameters(), lr=0.01), 
+    optimizer= kt.optim.Adam(lr=0.01)
 )
 
-x = tr.rand(1000, 3)
+x = tr.rand(1000, 7)
 
 y = tr.mm(
-    x, tr.Tensor([[-1, 1, -1]]).t()
+    x, tr.rand(1, 7).t()
 )
 
 dataset = TensorDataset(x, y)
