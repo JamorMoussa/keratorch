3d5346ce9ef871a1ff4d5da308ca4e07548eaca5
jamormoussa <moussajamorsup@gmail.com>
2024-11-16 03:21:50 +0100

    update test

--- test/test_module.py
+++ test/test_module.py
@@ -6,6 +6,12 @@ import keratorch as kt
 import matplotlib.pyplot as plt
 
 
+class PrintMessage(kt.callback.CallBack):
+
+    def on_batch_end(self, batch=None, logs = None):
+        print(logs)
+
+
 model = kt.nn.Sequential(
     kt.nn.Lambda(lambda x: x.unsqueeze(1)),
     nn.Conv1d(1, 10, kernel_size=2),
@@ -15,21 +21,21 @@ model = kt.nn.Sequential(
 
 model.compile(
     loss_fn= nn.MSELoss(),
-    optimizer= kt.optim.Adam(lr=0.01)
+    optimizer= kt.optim.Adam(lr=0.01),
+    callbacks= [PrintMessage(), ]
 )
 
 x = tr.rand(1000, 7)
 
 y = tr.mm(
     x, tr.rand(1, 7).t()
-)
+) + 0.3 * tr.randn(1000, 7)
 
 dataset = TensorDataset(x, y)
 
 loader = DataLoader(dataset, batch_size=25)
 
-res = model.fit(trloader= loader, num_iters=10)
+hist = model.fit(trloader= loader, num_iters=10)
 
-
-plt.plot(res["train_loss"])
+plt.plot(hist.history["train_loss"])
 plt.show()
