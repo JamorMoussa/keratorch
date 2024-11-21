e113a422865352916e6b674456e5c974b2e489fb
jamormoussa <moussajamorsup@gmail.com>
2024-11-21 19:36:53 +0100

    fix the progress bar message issue

--- keratorch/utils/iters.py
+++ keratorch/utils/iters.py
@@ -9,6 +9,7 @@ class TqdmIterator:
 
     loader: "DataLoader"
     tqdm_iter: tqdm
+    desc: str = None
 
     metrics: dict[str, str] = {}
 
@@ -17,10 +18,11 @@ class TqdmIterator:
 
     def get_tqdm(self, loader: "DataLoader", enum=False):
         self.loader = loader
-        self.tqdm_iter = tqdm(iterable=loader, desc=self.get_desc()) 
+        self.tqdm_iter = tqdm(iterable=loader)
 
         return enumerate(self.tqdm_iter) if enum else self.tqdm_iter
 
-    def get_desc(self,):
-        return " | ".join([f"{key}: {val}" for key, val in self.metrics.items()])
+    def update(self):
+        self.desc = " | ".join([f"{key}: {val}" for key, val in self.metrics.items()])
+        self.tqdm_iter.set_description(self.desc)
 
