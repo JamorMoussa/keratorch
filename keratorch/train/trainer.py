f689c44792ae8f6625423fd991ccbd57f372985d
jamormoussa <moussajamorsup@gmail.com>
2024-12-16 01:27:55 +0100

    add train method ktTrainer

--- keratorch/train/trainer.py
+++ keratorch/train/trainer.py
@@ -1,9 +1,13 @@
 from ..state import ktState
 from ..callbacks import CallBackList
+from ..outs import ModelOutput
 
+import keratorch as kt
 
-from abc import ABC, abstractmethod
+from typing import Any 
+from abc import ABC
 
+import torch 
 
 __all__ = ["ktTrainer", ]
 
@@ -18,15 +22,23 @@ class ktTrainer(ABC):
 
     def compile(
         self, 
-        model: 'kt.nn.ktModule', 
-        optimizer: 'kt.optim.Optimizer', 
-        loss_fn: 'Loss', 
+        model: kt.nn.ktModule, 
+        optimizer: kt.optim.ktOptimizer, 
+        loss_fn: kt.nn.ktLoss | torch.nn.modules.loss._Loss, 
+        *, 
+        device: torch.device = torch.device("cpu"),
         metrics: 'List[Metric]' = [],
         callbacks: 'List[Callback]' = []
     ):
         self.state.update(
-            model = model
+            model= model, optimizer= optimizer, loss_fn= loss_fn,  device= device
         )
+        self.state.model.to(self.state.device)
+        self.state.optimizer.set_params(
+            params= self.state.model.parameters()
+        )
+
+        self.callbacks.train.append(*callbacks)
 
     def train(
         self, 
@@ -35,7 +47,74 @@ class ktTrainer(ABC):
         *, 
         callbacks: 'List[Callback]'= [] 
     ):
-        raise NotImplementedError
+        self.state.model.train()
+        self.callbacks.train.on_train_begin()
+
+        for epoch in range(epochs):
+            
+            self.state.hyparams.update(epoch= epoch)
+            
+            self.callbacks.train.on_epoch_begin()
+
+            for itr, batch in enumerate(trainloader):
+            
+                self.state.hyparams.update(itr= itr)
+                self.state.train.update(batch= batch)
+
+                self.callbacks.train.on_batch_begin()
+
+                self.do_zero_grad()
+
+                outs: ModelOutput = self.do_forward_pass(batch= batch)
+
+                self.state.train.update(outputs= outs)
+                self.check_outputs_type(outputs= outs)
+
+                loss = self.compute_loss(outputs= outs.outputs, targets= batch[1])
+
+                self.do_backward_pass(loss= loss)
+
+                self.do_optimizer_step()
+
+                self.callbacks.train.on_batch_end()
+
+            self.callbacks.train.on_epoch_end()
+
+
+    def check_outputs_type(self, outputs: ModelOutput | Any):
+        if not isinstance(outputs, ModelOutput):
+            raise TypeError(f"model's outputs must be of type 'ModelOutput', got '{type(outputs).__name__}' instead.")
+
+
+    def do_zero_grad(self):
+        self.state.optimizer.zero_grad()
+
+
+    def do_forward_pass(
+        self, batch: tuple[torch.Tensor]
+    ) -> ModelOutput:
+        inputs = batch[0].to(self.state.device)
+
+        return self.state.model(inputs)
+
+
+    def compute_loss(
+        self, outputs: torch.Tensor, targets: torch.Tensor 
+    ): 
+        loss =  self.state.loss_fn(outputs, targets) 
+        self.state.train.update(loss= loss)
+
+        return loss 
+    
+
+    def do_backward_pass(
+        self, loss: torch.Tensor
+    ):
+        loss.backward() 
+
+
+    def do_optimizer_step(self):
+        self.state.optimizer.step()
 
 
     def evaluate(
--- keratorch/utils/validations.py
+++ keratorch/utils/validations.py
@@ -17,22 +17,22 @@ class TypeChecker:
 
             if field_info.name == name:
 
-                if hasattr(ftype, '__origin__') and ftype.__origin__ is tuple:
-                    if not isinstance(value, tuple):
+                if hasattr(ftype, '__origin__') and ftype.__origin__ is list:
+                    if not isinstance(value, list):
                         raise TypeError(
-                            f"'{self.__class__.__name__}.{fname}' must be a tuple, "
+                            f"'{self.__class__.__name__}.{fname}' must be a list, "
                             f"but got {type(value).__name__}."
                         )
                     
                     elif ftype.__args__ and not all(isinstance(v, ftype.__args__[0]) or v is None for v in value):
                         raise TypeError(
-                            f"'{self.__class__.__name__}.{fname}' must be a tuple of {ftype.__args__[0].__name__}, "
-                            f"but got a tuple with elements of type {[type(v).__name__ for v in value]}."
+                            f"'{self.__class__.__name__}.{fname}' must be a list of {ftype.__args__[0].__name__}, "
+                            f"but got a list with elements of type {[type(v).__name__ for v in value]}."
                         )
                     
                     elif len(value) != 2:
                         raise ValueError(
-                            f"The 'value' must be a tuple of two elements (inputs, targets), but got {len(value)} instead."
+                            f"The 'value' must be a list of two elements (inputs, targets), but got {len(value)} instead."
                         )
 
                 elif not isinstance(value, ftype) and value is not None:
